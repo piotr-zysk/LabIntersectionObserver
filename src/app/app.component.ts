37e2378a0d1d0afead3412612470d86a6bd55234
Piotr Zysk <pizy123@gmail.com>
2020-06-05 20:09:48 +0200

    get item and viewport height

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'LabIntersectionObserver';
+
+  observedItemOffsetHeight = 0;
+  observedItemOffsetTop = 0;
+
+  viewportHeightWithScrollBar = 0;
+  viewportHeightWithoutScrollBar = 0;
+
+  ngOnInit() {
+    this.getObserverItemPosition();
+    this.getViewPortHeight();
+  }
+
+  private getObserverItemPosition() {
+    const item = document.getElementById('observed');
+    this.observedItemOffsetHeight = item.offsetHeight;
+    this.observedItemOffsetTop = item.offsetTop;
+  }
+
+  private getViewPortHeight() {
+    this.viewportHeightWithScrollBar = window.innerHeight;
+    this.viewportHeightWithoutScrollBar = document.documentElement.clientHeight;
+  }
 }
